052dc5d07febcdfbe341958851d07fbd661192e8
resteve <resteve@zikzakmedia.com>
2015-11-29 21:59:30 +0100

    pyflakes

--- manifest.py
+++ manifest.py
@@ -1,7 +1,6 @@
 # This file is part of carrier_send_shipments_mrw module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from envialia import Picking
 from trytond.pool import PoolMeta
 
 __all__ = ['CarrierManifest']
