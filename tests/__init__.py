0e676a3e7c97ec81a411b791c56f3b9e712d80f8
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:20 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file is part of the carrier_send_shipments_mrw module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-
-from .test_carrier_send_shipments_mrw import suite
-
-__all__ = ['suite']
